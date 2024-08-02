5a26473541b6555cc727fe4fbb68e2cf34ea88fa
Lenchen <support@gsv2.dev>
2024-08-02 13:27:07 +0200

    .
    .

--- Team/special.py
+++ Team/special.py
@@ -4,8 +4,14 @@ from discord.commands import slash_command, Option
 
 OPTIONS = ['Voice', 'Deathchat', 'Team', 'Admin']
 
+
 class Special(commands.Cog):
+    file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
+    color = 0x2596be
+    embed = discord.Embed(color=color)
+
     def __init__(self, bot):
+        self.embed.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
         self.bot = bot
 
     @slash_command(description="Pings command with multiple options.")
@@ -23,29 +29,25 @@ class Special(commands.Cog):
 
     async def deathchat(self, ctx: discord.ApplicationContext):
         message = '<@&1014881120921321543>'
-        file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
-        color = 0x2596be
-        embed = discord.Embed(title='💀| Deathchatping |🔔', description=f'{ctx.author.name} hat den ping ausgeführt\n'
-                                                                   f'\n'
-                                                                   f'Dieser ping ist abstellbar in den <#1073993336890871848>\n'
-                                                                   f'PS wir nehmen uns das recht raus sollte dieser ping nix bringen, andere zu pingen\n'
-                                                                   f'Hate etc wird schwerstens bestraft, (Keine Angst, wir sind Hard aber Fair)', colour=color)
-        embed.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
+        self.embed.title = '💀| Deathchatping |🔔'
+        self.embed.description = f'''{ctx.author.name} hat den ping ausgeführt\n'
+                                 \nDieser ping ist abstellbar in den <#1073993336890871848>
+                                 \nPS wir nehmen uns das recht raus sollte dieser ping nix bringen, andere zu pingen
+                                 \nHate etc wird schwerstens bestraft, (Keine Angst, wir sind Hard aber Fair)'''
         await ctx.send(message)
-        await ctx.respond(file=file, embed=embed)
+        await ctx.respond(file=self.file, embed=self.embed)
 
     async def team(self, ctx: discord.ApplicationContext):
         if 1070336143373119593 not in [role.id for role in ctx.author.roles]:
-            await self.send_error(ctx, '"Dies ist eine Teamfunktion, bewerbe dich gerne fürs Team unter: <#1073702332082172084>"')
+            await self.send_error(ctx,
+                                  '"Dies ist eine Teamfunktion, bewerbe dich gerne fürs Team unter: <#1073702332082172084>"')
             return
 
         message = '<@&1044557317947019264>'
-        file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
-        color = 0x2596be
-        embed = discord.Embed(title='💀| Teamping |🔔', description=f'{ctx.author.name} hat den ping ausgeführt', colour=color)
-        embed.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
+        self.embed.title = '💀| Teamping |🔔'
+        self.embed.description = f'{ctx.author.name} hat den ping ausgeführt'
         await ctx.send(message)
-        await ctx.respond(file=file, embed=embed)
+        await ctx.respond(file=self.file, embed=self.embed)
 
     async def admin(self, ctx: discord.ApplicationContext):
         if 1044557317947019264 not in [role.id for role in ctx.author.roles]:
@@ -53,51 +55,41 @@ class Special(commands.Cog):
             return
 
         message = '<@696282645100888086>'
-        file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
-        color = 0x2596be
-        embed = discord.Embed(title='💀| Adminping |🔔', description=f'{ctx.author.name} hat den ping ausgeführt', colour=color)
-        embed.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
+        self.embed.title = '💀| Adminping |🔔'
+        self.embed.description = f'{ctx.author.name} hat den ping ausgeführt'
         await ctx.send(message)
-        await ctx.respond(file=file, embed=embed)
+        await ctx.respond(file=self.file, embed=self.embed)
 
     async def voice(self, ctx: discord.ApplicationContext):
         message = '<@&1248706499539238956>'
-        file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
-        color = 0x2596be
-        embed = discord.Embed(title='🔊| Voiceping |🔔', description=f'{ctx.author.name} hat den ping ausgeführt\n'
-                                                                   'Wie es aussieht sucht da jemand nen call\n'
-                                                                   'Hoffe wir finden schnell Leute die sich anschließen <3', colour=color)
-        embed.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
+        self.embed.title = '🔊| Voiceping |🔔'
+        self.embed.description = f'''{ctx.author.name} hat den ping ausgeführt\n
+                             Wie es aussieht sucht da jemand nen call\n
+                             Hoffe wir finden schnell Leute die sich anschließen <3'''
         await ctx.send(message)
-        await ctx.respond(file=file, embed=embed)
+        await ctx.respond(file=self.file, embed=self.embed)
 
     async def send_error(self, ctx: discord.ApplicationContext, error_message: str):
-        file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
         em = discord.Embed(title=' ❌| Error | ❌', description=error_message, color=discord.Color.red())
-        em.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
-        await ctx.send(file=file, embed=em, delete_after=15)
+        em.set_footer(text=self.embed.footer.text, icon_url=self.embed.footer.icon_url)
+        await ctx.send(file=self.file, embed=em, delete_after=15)
 
     @commands.slash_command(description='📝┃Sende die Liste der Server-Emojis')
     async def emojilist(self, ctx: discord.ApplicationContext):
-        file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
-        color = 0x2596be
         embed = discord.Embed(
             title='Emojis',
             description='',
-            color=color)
-        embed.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
+            color=self.embed.color)
+        embed.set_footer(text=self.embed.footer.text, icon_url=self.embed.footer.icon_url)
 
         if len(ctx.guild.emojis) > 50:
-            embed_2 = discord.Embed(
-                title='❌ | Error | ❌',
-                description='Anscheinend gibt es zu viele Emojis auf diesem Server :( \nIch bin dadurch leider etwas eingeschränkt',
-                color=discord.Color.dark_red())
-            embed_2.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
-            await ctx.respond(file=file, embed=embed_2, delete_after=120)
+            embed.title = '❌ | Error | ❌',
+            embed.description = 'Anscheinend gibt es zu viele Emojis auf diesem Server :( \nIch bin dadurch leider etwas eingeschränkt',
+            embed.color = discord.Color.dark_red()
         else:
             for emoji in ctx.guild.emojis:
                 embed.description += f'{emoji} - `{emoji}`\n'
-            await ctx.respond(file=file, embed=embed, delete_after=120)
+        await ctx.respond(file=self.file, embed=embed, delete_after=120)
 
     @commands.slash_command(description='.')
     @commands.has_permissions(administrator=True)
@@ -110,13 +102,13 @@ class Special(commands.Cog):
     @coming_soon.error
     async def coming_soon_error(self, ctx: discord.ApplicationContext, error):
         if isinstance(error, commands.MissingPermissions):
-            file = discord.File("img/GSv_Logo_ai.png", filename='GSv_Logo.png')
             em = discord.Embed(title=' ❌| Error | ❌',
                                description="Leider ist dies eine Admin Option.\nWenn du Hilfe brauchst, melde dich gerne im Support:\n<#1073700885886152837>",
                                color=discord.Color.red())
             em.set_footer(text="Powered by gsv2.dev ⚡", icon_url="attachment://GSv_Logo.png")
-            await ctx.send(file=file, embed=em, delete_after=15)
+            await ctx.send(file=self.file, embed=em, delete_after=15)
             print(f'{ctx.author.name} hat versucht /coming_soon auszuführen')
+
 
 def setup(bot):
     bot.add_cog(Special(bot))
